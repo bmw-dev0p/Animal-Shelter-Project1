7327196aa6ab793004d9d209ce068e1b20192a00
SirrBarnes <torressergio1357@gmail.com>
2024-07-10 16:53:47 -0400

    form data gets saved to local storage and added readme

---
+++ assets/scripts/form-script.js
@@ -0,0 +1,21 @@
+const submitButton = document.getElementById("submit");
+const petName = document.getElementById("petName");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const animalChoice = document.querySelector("input[type=radio]").checked;
+
+function submitData(event) {
+    event.preventDefault();
+
+    const fullName = `${firstName.value} ${lastName.value}`;
+
+    localStorage.setItem("AdopterName", fullName);
+    localStorage.setItem("petName", petName.value);
+
+    
+
+    console.log(animalChoice);
+}
+
+
+submitButton.addEventListener("click", submitData);
